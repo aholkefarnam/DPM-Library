5f7c2e5a3a7025daa0862567cf25ac45cc5ad0f3
Anna (Josie) Holke-Farnam <anna-holke-farnam@uiowa.edu>
2023-04-04 11:11:28 -0500

    Updated to copy Adam's files

--- app.py
+++ app.py
@@ -1,43 +1,71 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, flash
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "aj-dpm"
 
 friends_dict = [
-    {"name": "Test", "flavor": "swirl", "read": "yes", "activities": "reading"}
+    {"title": "The Hobbit", 
+     "author": "J. R. R. Tolkien", 
+     "pages": "310", 
+     "type": "nonfiction",
+     "details": "own",
+     "acquire": "purchase"}
 ]
 
+# Handling error 404 and displaying relevant web page
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template("404.html"), 404
 
-@app.route("/", methods=["GET", "POST"])
+# Handling error 500 and displaying relevant web page
+@app.errorhandler(500)
+def not_found_error(error):
+    return render_template("500.html"), 500
+
+@app.route('/', methods=["GET", "POST"])
 def index():
     return render_template(
         "index.html", pageTitle="Web form template", friends=friends_dict
     )
 
+@app.route('/about', methods=["GET","POST"])
+def about():
+    return render_template(
+        "about.html", pageTitle="About", friends=friends_dict
+    )
 
-@app.route("/add", methods=["POST"])
+
+
+@app.route('/add', methods=["GET", "POST"])
 def add():
     print("inside add function")
     if request.method == "POST":
 
         form = request.form
 
-        fname = form["fname"]
-        flavor = form["flavor"]
-        read = form["read"]
-        activities = form.getlist("activities")  # this is a PYthon list
+        title = form["title"]
+        author = form["author"]
+        pages = form["pages"]
+        type = form["type"]
+        details = form.getlist("details")  # this is a Python list
+        acquire = form["acquire"]
 
-        print(fname)
-        print(flavor)
-        print(read)
-        print(activities)
+        print(title)
+        print(author)
+        print(pages)
+        print(type)
+        print(details)
+        print(acquire)
 
-        activities_string = ", ".join(activities)  # make the Python list into a string
+        details_string = ", ".join(details)  # make the Python list into a string
 
         friend_dict = {
-            "name": fname,
-            "flavor": flavor,
-            "read": read,
-            "activities": activities_string,
+            "title": title,
+            "author": author,
+            "pages": pages,
+            "type": type,
+            "details": details_string,
+            "acquire": acquire
         }
 
         print(friend_dict)
@@ -45,6 +73,9 @@ def add():
             friend_dict
         )  # append this dictionary entry to the larger friends dictionary
         print(friends_dict)
+
+        flash('The book ' + title + ' has been added. ', 'success')
+
         return redirect(url_for("index"))
     else:
         return redirect(url_for("index"))
